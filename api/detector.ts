5f9a26f7212313eb8eccb847e8b36327af42b6d5
gxfn33157 <geovanne.fonseca33@gmail.com>
2025-12-25 12:11:54 -0300

    Update detector.ts

--- api/detector.ts
+++ api/detector.ts
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: "nodejs"
+};
+
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import dns from "dns/promises";
 import net from "net";
@@ -119,3 +123,4 @@ export default async function handler(
     timestamp: new Date().toISOString()
   });
 }
+
